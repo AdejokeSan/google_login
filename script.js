b9b37360c0129ac5c1e8d71d41383c064ee9cd94
AdejokeSan <djksanusi@gmail.com>
2023-08-22 08:09:18 -0700

    google login page

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const Password = document.querySelector("#password");
+const Checkbox = document.querySelector("#show");
+
+
+Checkbox.addEventListener('click',function(){
+  const type =Password.getAttribute("type")=== "password" ? "text" : "password";
+  Password.setAttribute("type",type);
+});
